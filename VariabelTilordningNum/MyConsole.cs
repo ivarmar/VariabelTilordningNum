e3de2ccc95993bec7dea87238b0e32dcbdc34284
ivarmar <141323294+ivarmar@users.noreply.github.com>
2023-11-15 13:22:50 +0100

    Add project files.

---
+++ VariabelTilordningNum/MyConsole.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gouldavan
+{
+    internal class MyConsole
+    {
+
+        public int MathNumbers()
+            {
+                Console.Write("Enter the first number: ");
+                int input1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter the second number: ");
+                int input2 = int.Parse(Console.ReadLine());
+
+                var sum = input1 + input2;
+                Console.WriteLine($"You entered {input1} and {input2}");
+                return sum;
+            }
+
+        public static string Nothing()
+        {
+            Console.WriteLine("This Method does basically nothing");
+            return String.Empty;
+        }
+        
+
+
+    }
+}
---
+++ VariabelTilordningNum/Program.cs
@@ -0,0 +1,25 @@
+﻿using gouldavan;
+namespace VariabelTilordningNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                MyConsole mathInstance = new MyConsole();
+
+                Console.WriteLine($"The total is: {mathInstance.MathNumbers()}");
+
+                var Empty = MyConsole.Nothing();
+                Console.WriteLine($"{Empty}");
+            }
+
+
+        }
+
+
+
+
+    }
+}
